540e7c2ae381eef2ed5910a521217b4b3f775f82
ryan_gilles <ryan@e-monsite.com>
2017-03-20 13:19:44 +0000

    WIP

--- src/Rygilles/ApiDoc/Generators/DingoGenerator.php
+++ src/Rygilles/ApiDoc/Generators/DingoGenerator.php
@@ -35,6 +35,43 @@ class DingoGenerator extends \Mpociot\ApiDoc\Generators\DingoGenerator
 	}
 
 	/**
+	 * @param \Illuminate\Routing\Route $route
+	 * @param array $bindings
+	 * @param array $headers
+	 * @param bool $withResponse
+	 *
+	 * @return array
+	 */
+	public function processRoute($route, $bindings = [], $headers = [], $withResponse = true)
+	{
+		$response = '';
+
+		if ($withResponse) {
+			try {
+				$response = $this->getRouteResponse($route, $bindings, $headers);
+			} catch (Exception $e) {
+			}
+		}
+
+		dd($response);
+
+		$routeAction = $route->getAction();
+		$routeGroup = $this->getRouteGroup($routeAction['uses']);
+		$routeDescription = $this->getRouteDescription($routeAction['uses']);
+
+		return $this->getParameters([
+			'id' => md5($route->uri().':'.implode($route->getMethods())),
+			'resource' => $routeGroup,
+			'title' => $routeDescription['short'],
+			'description' => $routeDescription['long'],
+			'methods' => $route->getMethods(),
+			'uri' => $route->uri(),
+			'parameters' => [],
+			'response' => $response,
+		], $routeAction, $bindings);
+	}
+
+	/**
 	 * {@inheritdoc}
 	 */
 	public function callRoute($method, $uri, $parameters = [], $cookies = [], $files = [], $server = [], $content = null)
@@ -58,7 +95,7 @@ class DingoGenerator extends \Mpociot\ApiDoc\Generators\DingoGenerator
 		});
 
 		// Rygilles : Extra console output logs
-		$this->getParentCommand()->comment("\r\n" . 'Calling route [method="' . $method . '", "uri=' . ltrim($uri, '/') . '", parameters=["' . implode('", "', $parameters) . '"]' /* with headers : ' . "\r\n" . print_r($server, true) . "\r\n"*/);
+		$this->getParentCommand()->comment("\r\n" . 'Calling route (method="' . $method . '", "uri=' . ltrim($uri, '/') . '", parameters=["' . implode('", "', $parameters) . '"])' /* with headers : ' . "\r\n" . print_r($server, true) . "\r\n"*/);
 
 		try {
 			$resp = call_user_func_array([$dispatcher, strtolower($method)], [$uri]);
